74f5a6bd795c7d1066fc1890c674e5f8f000bc73
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-10 00:19:24 +0530

    Update and rename python_module6.py to python_lesson1_module6.py

---
+++ python_lesson1_module6.py
@@ -0,0 +1,51 @@
+print("Hello world!")
+
+print("Anuroop Mishra")
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Cereals and Chapati"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal="Fried Rice"
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+print('This message has mismatched quote marks!')
+print('Abracadabra')
+
+# Define the release and runtime integer variables below:
+release_year=2020
+runtime=105
+
+# Define the rating_out_of_10 float variable below:
+rating_out_of_10=7.9
+
+print(25 * 68 + 13/28)
+
+quilt_width=8
+quilt_length=12
+print(quilt_width * quilt_length)
+quilt_length=8
+print(quilt_width * quilt_length)
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+# 7x7 quilt
+print(7 ** 2)
+# 8x8 quilt
+print(8 ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6**4)
