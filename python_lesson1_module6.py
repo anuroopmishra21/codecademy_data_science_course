faecd07989820de1cfbcbc3179549f5fc12e7255
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-10 00:35:55 +0530

    Update python_lesson1_module6.py

--- python_lesson1_module6.py
+++ python_lesson1_module6.py
@@ -4,18 +4,14 @@ print("Anuroop Mishra")
 
 # We've defined the variable "meal" here to the name of the food we ate for breakfast!
 meal = "An english muffin"
-
 # Printing out breakfast
 print("Breakfast:")
 print(meal)
-
 # Now update meal to be lunch!
 meal = "Cereals and Chapati"
-
 # Printing out lunch
 print("Lunch:")
 print(meal)
-
 # Now update "meal" to be dinner
 meal="Fried Rice"
 # Printing out dinner
@@ -28,7 +24,6 @@ print('Abracadabra')
 # Define the release and runtime integer variables below:
 release_year=2020
 runtime=105
-
 # Define the rating_out_of_10 float variable below:
 rating_out_of_10=7.9
 
@@ -49,3 +44,37 @@ print(7 ** 2)
 print(8 ** 2)
 # How many squares for 6 people to have 6 quilts each that are 6x6?
 print(6**4)
+
+my_team=27%4
+print(my_team)
+print(26%4)
+print(28%4)
+
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+# Define message below:
+message=string1+string2+string3+string4+string5+string6
+print(message)
+
+total_price = 0
+new_sneakers = 50.00
+total_price += new_sneakers
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price+=nice_sweater+fun_books
+print("The total price is", total_price)
+
+# Assign the string here
+to_you='''Stranger, if you passing meet me and desire to speak to me, why should you not speak to me? And why should I not speak to you?'''
+print(to_you)
+
+my_age=20
+half_my_age=my_age/2
+greeting='Hello there I am '
+name='Anuroop Mishra'
+greeting_with_name=greeting+name
