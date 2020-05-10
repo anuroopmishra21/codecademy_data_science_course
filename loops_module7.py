2c27a1209d4190a4acaca308b23bebb183091d31
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-11 01:22:32 +0530

    Update loops_module7.py

--- loops_module7.py
+++ loops_module7.py
@@ -8,3 +8,21 @@ for game in board_games:
   print(game)
 for sport in sport_games:
   print(sport)
+
+promise = "I will not chew gum in class"
+for i in range(5):
+  print(promise)
+  
+students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
+students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
+for student in students_period_A:
+  students_period_B.append(student)
+  print(student)
+  
+dog_breeds_available_for_adoption = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+dog_breed_I_want = 'dalmatian'
+for breed in dog_breeds_available_for_adoption:
+  print(breed)
+  if breed == dog_breed_I_want:
+    print('They have the dog I want!')
+    break
