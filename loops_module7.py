6829cc05776e675b566e9c58cff8d6f0e889fc1c
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-11 01:12:51 +0530

    Create loops_module7.py

---
+++ loops_module7.py
@@ -0,0 +1,10 @@
+dog_breeds = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+for breed in dog_breeds:
+  print(breed)
+  
+board_games = ['Settlers of Catan', 'Carcassone', 'Power Grid', 'Agricola', 'Scrabble']
+sport_games = ['football', 'football - American', 'hockey', 'baseball', 'cricket']
+for game in board_games:
+  print(game)
+for sport in sport_games:
+  print(sport)
