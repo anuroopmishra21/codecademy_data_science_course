621cf14a38b97590456396b7ffb1aaf5ca637b90
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-11 00:08:06 +0530

    Create lists_module7.py

---
+++ lists_module7.py
@@ -0,0 +1,22 @@
+heights = [61, 70, 67, 64, 65]
+broken_heights = [65, 71 ,59, 62]
+
+ints_and_strings = [1, 2, 3, 'four', 'five','six']
+sam_height=['Sam',67]
+
+heights = [['Jenny', 61], ['Alexus', 70], ['Sam', 67], ['Grace', 64], ['Vik', 68]]
+ages=[['Aaron',15],['Dhruti',16]]
+
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+names_and_dogs_names=zip(names,dogs_names)
+list_of_names_and_dogs_names=list(names_and_dogs_names)
+print(list_of_names_and_dogs_names)
+
+my_empty_list=[]
+
+orders = ['daisies', 'periwinkle']
+print(orders)
+orders.append('tulips')
+orders.append('roses')
+print(orders)
