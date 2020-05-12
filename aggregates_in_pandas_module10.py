e6df259ab44a752665cddd6a6c46b5ebb83f763c
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-13 01:45:02 +0530

    Update aggregates_in_pandas_module10.py

--- aggregates_in_pandas_module10.py
+++ aggregates_in_pandas_module10.py
@@ -44,3 +44,17 @@ shoe_counts_pivot = shoe_counts.pivot(
   values = 'id'
 ).reset_index()
 print(shoe_counts_pivot)
+
+import codecademylib
+import pandas as pd
+user_visits = pd.read_csv('page_visits.csv')
+print(user_visits.head())
+click_source = user_visits.groupby('utm_source').id.count().reset_index()
+print(click_source)
+click_source_by_month = user_visits.groupby(['utm_source','month']).id.count().reset_index()
+click_source_by_month_pivot = click_source_by_month.pivot(
+  columns = 'month',
+  index = 'utm_source',
+  values = 'id'
+).reset_index()
+print(click_source_by_month_pivot)
