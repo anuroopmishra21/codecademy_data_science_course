0c557ed19cdcf2167aa3e7fb2191b74670dc2ff0
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-13 00:31:44 +0530

    Update modifying_data_frames_module10.py

--- modifying_data_frames_module10.py
+++ modifying_data_frames_module10.py
@@ -61,3 +61,34 @@ df = pd.read_csv('employees.csv')
 get_last_name = lambda x:x.split(' ')[-1]
 df['last_name'] = df.name.apply(get_last_name)
 print(df)
+
+import codecademylib
+import pandas as pd
+df = pd.read_csv('employees.csv')
+total_earned = lambda row: (row.hourly_wage * 40) + ((row.hourly_wage * 1.5) * (row.hours_worked - 40)) \
+	if row.hours_worked > 40 \
+  else row.hourly_wage * row.hours_worked
+df['total_earned'] = df.apply(total_earned, axis = 1)
+print(df)
+
+import codecademylib
+import pandas as pd
+df = pd.read_csv('imdb.csv')
+df.columns = ['ID','Title','Category','Year Released','Rating']
+print(df)
+
+import codecademylib
+import pandas as pd
+df = pd.read_csv('imdb.csv')
+df.rename(columns={'name':'movie_title'},inplace=True)
+print(df)
+
+import codecademylib
+import pandas as pd
+orders = pd.read_csv('shoefly.csv')
+print(orders.head())
+orders['shoe_source'] = orders.shoe_material.apply(lambda x: 'animal' if x == 'leather'
+else 'vegan')
+orders['salutation'] = orders.apply(lambda row: 'Dear Mr. {}'.format(row.last_name) if row.gender=='male'
+else 'Dear Ms. {}'.format(row.last_name),
+axis = 1)
