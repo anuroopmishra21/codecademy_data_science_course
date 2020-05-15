ecf7ad26454171a0dc5d3deb5ef713420e686566
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-16 02:16:19 +0530

    Update foodwheel_project3_module12.py

--- foodwheel_project3_module12.py
+++ foodwheel_project3_module12.py
@@ -47,3 +47,15 @@ plt.ylabel('Average Price')
 plt.xlabel('Months')
 plt.title('Average Price in Each Month')
 plt.show()
+
+import codecademylib
+from matplotlib import pyplot as plt
+import pandas as pd
+orders = pd.read_csv('orders.csv')
+customer_amount = orders.groupby('customer_id').price.sum().reset_index()
+print(customer_amount.head())
+plt.hist(customer_amount.price.values,range=(0, 200), bins=40)
+plt.xlabel('Total Spent')
+plt.ylabel("Number of Customers")
+plt.title('Customer Expenditure Over 6 Months')
+plt.show()
