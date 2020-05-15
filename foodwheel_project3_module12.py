c750d6ae54e0a2c4101776aad8886df6d7c76291
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-16 02:03:46 +0530

    Update foodwheel_project3_module12.py

--- foodwheel_project3_module12.py
+++ foodwheel_project3_module12.py
@@ -6,3 +6,44 @@ cuisine_options_count=restaurants.cuisine.nunique()
 cuisine_counts=restaurants.groupby('cuisine').name.count().reset_index()
 print(cuisine_options_count)
 print(cuisine_counts)
+
+import codecademylib3
+from matplotlib import pyplot as plt
+import pandas as pd
+restaurants = pd.read_csv('restaurants.csv')
+cuisine_counts = restaurants.groupby('cuisine').name.count().reset_index()
+cuisines = cuisine_counts.cuisine.values
+counts = cuisine_counts.name.values
+plt.pie(counts,autopct='%d%%',labels=cuisines)
+plt.title('Number of restaurants for each cuisine')
+plt.axis('equal')
+plt.show()
+
+import codecademylib
+from matplotlib import pyplot as plt
+import pandas as pd
+orders = pd.read_csv('orders.csv')
+print orders.head()
+orders['month'] = orders.date.apply(lambda x: x.split('-')[0])
+print orders.head()
+avg_order = orders.groupby('month').price.mean().reset_index()
+std_order = orders.groupby('month').price.std().reset_index()
+
+import codecademylib
+from matplotlib import pyplot as plt
+import pandas as pd
+orders = pd.read_csv('orders.csv')
+orders['month'] = orders.date.apply(lambda x: x.split('-')[0])
+avg_order = orders.groupby('month').price.mean().reset_index()
+std_order = orders.groupby('month').price.std().reset_index()
+ax=plt.subplot()
+bar_heights = [i for i in avg_order.price]
+bar_errors = [i for i in std_order.price]
+months = ['April','May','June','July','August','September']
+plt.bar(range(len(bar_heights)),bar_heights,yerr=bar_errors,capsize=5)
+ax.set_xticks(range(len(bar_heights)))
+ax.set_xticklabels(months)
+plt.ylabel('Average Price')
+plt.xlabel('Months')
+plt.title('Average Price in Each Month')
+plt.show()
