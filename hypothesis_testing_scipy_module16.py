6d3b5bc4f4ebc4e7e9325a21ecb9a04ae46ec60f
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-24 01:14:45 +0530

    Update hypothesis_testing_scipy_module16.py

--- hypothesis_testing_scipy_module16.py
+++ hypothesis_testing_scipy_module16.py
@@ -98,3 +98,25 @@ v = np.concatenate([a, b, c])
 labels = ['a'] * len(a) + ['b'] * len(b) + ['c'] * len(c)
 tukey_results = pairwise_tukeyhsd(v,labels,0.05)
 print tukey_results
+
+from scipy.stats import binom_test
+pval = binom_test(510, n=10000, p=0.06)
+print pval
+pval2 = binom_test(590, n=10000, p=0.06)
+print pval2
+
+from scipy.stats import chi2_contingency
+
+# Contingency table
+#         harvester |  leaf cutter
+# ----+------------------+------------
+# 1st gr | 30       |  10
+# 2nd gr | 35       |  5
+# 3rd gr | 28       |  12
+
+X = [[30, 10],
+     [35, 5],
+     [28, 12],
+     [20,20]]
+chi2, pval, dof, expected = chi2_contingency(X)
+print pval
