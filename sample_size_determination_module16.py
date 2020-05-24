35473bcc3fd7e493272b88b130c634cc4a0f7ed5
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-24 23:21:05 +0530

    Create sample_size_determination_module16.py

---
+++ sample_size_determination_module16.py
@@ -0,0 +1,25 @@
+sample_size=760
+
+number_of_site_visitors = 2000.0
+number_of_converted_visitors = 1300.0
+conversion_rate = number_of_converted_visitors/number_of_site_visitors
+print conversion_rate
+
+lift_eight_percent_to_twelve_percent = 100*(12-8)/8
+ten_percent_up_fifty_percent = 10.0+10.0*(50.0/100.0)
+
+hamster_headline_experiment_length = 910/(350 * 20.0/100.0)
+print hamster_headline_experiment_length
+
+flow_population = 100000
+outing_pop = 643
+
+import math
+margin_of_error = 4
+confidence_level = 95
+likely_proportion = 40
+population_size = 100000
+sample_size = 573
+weeks_of_survey = math.ceil(sample_size/120.0)
+
+band_battle_sample_size = 170
