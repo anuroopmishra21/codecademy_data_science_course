930afe2705325eb4062bdda6b5e49f5885ab6f29
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-05-28 00:04:17 +0530

    Update beautiful_soup_module19.py

--- beautiful_soup_module19.py
+++ beautiful_soup_module19.py
@@ -1,2 +1,24 @@
 from preprocess import turtles
 print(turtles)
+
+respect = 'I will not spam websites'
+
+import requests
+webpage_response = requests.get('https://s3.amazonaws.com/codecademy-content/courses/beautifulsoup/shellter.html')
+webpage = webpage_response.content
+print(webpage)
+
+import requests
+from bs4 import BeautifulSoup
+webpage_response = requests.get('https://s3.amazonaws.com/codecademy-content/courses/beautifulsoup/shellter.html')
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage,'html.parser')
+print(soup)
+
+import requests
+from bs4 import BeautifulSoup
+webpage_response = requests.get('https://s3.amazonaws.com/codecademy-content/courses/beautifulsoup/shellter.html')
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage, "html.parser")
+print(soup.p)
+print(soup.p.string)
