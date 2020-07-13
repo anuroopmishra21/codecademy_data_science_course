843fecabedf07970f4eefd8c6d09796bc49a9658
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-13 23:50:54 +0530

    Update knn_algorithm_module20.py

--- knn_algorithm_module20.py
+++ knn_algorithm_module20.py
@@ -185,3 +185,9 @@ def find_validation_accuracy(training_set,training_labels,validation_set,validat
   return validation_error
 print(find_validation_accuracy(training_set,training_labels,validation_set,validation_labels,3))
 
+from movies import movie_dataset, labels
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(movie_dataset,labels)
+print(classifier.predict([[.45,.2,.5],[.25,.8,.9],[.1,.1,.9]]))
+
