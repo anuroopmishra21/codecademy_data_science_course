aeb2a53037668514cb8501a95a2bf5ea638b1be7
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-15 17:36:10 +0530

    Update logistic_regression_module20.py

--- logistic_regression_module20.py
+++ logistic_regression_module20.py
@@ -67,3 +67,40 @@ print(loss_1)
 # Calculate and print loss_2 here
 loss_2 = log_loss(probabilities_2,passed_exam)
 print(loss_2)
+
+import numpy as np
+from exam import hours_studied, calculated_coefficients, intercept
+def log_odds(features, coefficients,intercept):
+  return np.dot(features,coefficients) + intercept
+def sigmoid(z):
+    denominator = 1 + np.exp(-z)
+    return 1/denominator
+# Create predict_class() function here
+def predict_class(features,coefficients,intercept,threshold):
+  calculated_log_odds = log_odds(features,coefficients,intercept)
+  probabilities = sigmoid(calculated_log_odds)
+  return np.where(probabilities >= threshold, 1, 0)
+# Make final classifications on Codecademy University data here
+final_results = predict_class(hours_studied,calculated_coefficients,intercept,0.5)
+print(final_results)
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from exam import hours_studied_scaled, passed_exam, exam_features_scaled_train, exam_features_scaled_test, passed_exam_2_train, passed_exam_2_test, guessed_hours_scaled
+# Create and fit logistic regression model here
+model = LogisticRegression()
+model.fit(hours_studied_scaled,passed_exam)
+calculated_coefficients = model.coef_
+intercept = model.intercept_
+print(calculated_coefficients)
+print(intercept)
+# Predict the probabilities of passing for next semester's students here
+passed_predictions = model.predict_proba(guessed_hours_scaled)
+print(passed_predictions)
+# Create a new model on the training data with two features here
+model_2 = LogisticRegression()
+model_2.fit(exam_features_scaled_train,passed_exam_2_train)
+# Predict whether the students will pass here
+passed_predictions_2 = model_2.predict(exam_features_scaled_test)
+print(passed_predictions_2)
+print(passed_exam_2_test)
