d7f8675d7faec5d451cd3cd4eccee7a5b2300594
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-17 15:45:17 +0530

    Update svm_module20.py

--- svm_module20.py
+++ svm_module20.py
@@ -75,3 +75,40 @@ classifier.fit(points, labels)
 draw_points(points, labels)
 draw_margin(classifier)
 plt.show()
+
+import codecademylib3_seaborn
+from sklearn.svm import SVC
+from graph import points, labels
+from sklearn.model_selection import train_test_split
+training_data, validation_data, training_labels, validation_labels = train_test_split(points, labels, train_size = 0.8, test_size = 0.2, random_state = 100)
+classifier = SVC(kernel = 'poly', degree = 2)
+classifier.fit(training_data,training_labels)
+print(classifier.score(validation_data,validation_labels))
+
+from sklearn.datasets import make_circles
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+#Makes concentric circles
+points, labels = make_circles(n_samples=300, factor=.2, noise=.05, random_state = 1)
+#Makes training set and validation set.
+training_data, validation_data, training_labels, validation_labels = train_test_split(points, labels, train_size = 0.8, test_size = 0.2, random_state = 100)
+classifier = SVC(kernel = "linear", random_state = 1)
+classifier.fit(training_data, training_labels)
+print(classifier.score(validation_data, validation_labels))
+print(training_data[0])
+new_training = []
+new_validation = []
+for point in training_data:
+  new_training.append([ (2**0.5)*point[0]*point[1] , point[0]**2 , point[1]**2])
+for point in validation_data:
+  new_validation.append([ (2**0.5)*point[0]*point[1] , point[0]**2 , point[1]**2])
+classifier.fit(new_training,training_labels)
+print(classifier.score(new_validation,validation_labels))
+
+from data import points, labels
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+training_data, validation_data, training_labels, validation_labels = train_test_split(points, labels, train_size = 0.8, test_size = 0.2, random_state = 100)
+classifier = SVC(kernel = 'rbf',gamma = 1)
+classifier.fit(training_data,training_labels)
+print(classifier.score(validation_data,validation_labels))
