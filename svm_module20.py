60ba12113945b23df87a583247afc3bd0b3e6d33
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-17 15:25:31 +0530

    Update svm_module20.py

--- svm_module20.py
+++ svm_module20.py
@@ -52,3 +52,26 @@ plt.show()
 red_support_vector = [1,6]
 blue_support_vector_one = [0.5, 2]
 blue_support_vector_two = [2.5, 2]
+margin_size = 2
+
+from sklearn.svm import SVC
+from graph import points, labels
+classifier = SVC(kernel = "linear")
+classifier.fit(points,labels)
+print(classifier.predict([[3,4],[6,7]]))
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from graph import points, labels, draw_points, draw_margin
+points.append([3,3])
+labels.append(0)
+points.append([1,11])
+labels.append(1)
+points.append([5,9])
+labels.append(1)
+classifier = SVC(kernel='linear', C = 0.2)
+classifier.fit(points, labels)
+draw_points(points, labels)
+draw_margin(classifier)
+plt.show()
