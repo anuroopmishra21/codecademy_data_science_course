3a6694c2f70fb70765daae457bbca0b06584771c
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-18 22:21:48 +0530

    Update decision_tree_module20.py

--- decision_tree_module20.py
+++ decision_tree_module20.py
@@ -95,3 +95,15 @@ def build_tree(data,labels):
   return branches
 tree = build_tree(car_data,car_labels)
 print_tree(tree)
+
+from tree import *
+import operator
+test_point = ['vhigh', 'low', '3', '4', 'med', 'med']
+def classify(datapoint,tree):
+  if isinstance(tree, Leaf):
+    return max(tree.labels.items(), key=operator.itemgetter(1))[0]
+  value = datapoint[tree.feature]
+  for branch in tree.branches:
+    if branch.value == value:
+      return classify(datapoint, branch)
+print(classify(test_point,tree))
