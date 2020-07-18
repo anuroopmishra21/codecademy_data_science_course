949f317e6d6f018df2973490fb477a97cd36484d
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-18 21:35:47 +0530

    Create decision_tree_module20.py

---
+++ decision_tree_module20.py
@@ -0,0 +1,39 @@
+from tree import tree, classify
+car = ['med','med','4','more','big','high']
+print(classify(car,tree))
+
+from collections import Counter
+labels = ["unacc", "unacc", "acc", "acc", "good", "good"]
+#labels = ["unacc","unacc","unacc", "good", "vgood", "vgood"]
+#labels = ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc"]
+impurity = 1
+label_counts = Counter(labels)
+print(label_counts)
+for label in label_counts:
+  probability_of_label = label_counts[label]/len(labels)
+  impurity -= probability_of_label**2
+print(impurity)
+
+from collections import Counter
+unsplit_labels = ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc", "good", "good", "good", "good", "vgood", "vgood", "vgood"]
+split_labels_1 = [
+  ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc", "good", "good", "vgood"], 
+  [ "good", "good"], 
+  ["vgood", "vgood"]
+]
+split_labels_2 = [
+  ["unacc", "unacc", "unacc", "unacc","unacc", "unacc", "good", "good", "good", "good"], 
+  ["vgood", "vgood", "vgood"]
+]
+def gini(dataset):
+  impurity = 1
+  label_counts = Counter(dataset)
+  for label in label_counts:
+    prob_of_label = label_counts[label] / len(dataset)
+    impurity -= prob_of_label ** 2
+  return impurity
+info_gain = gini(unsplit_labels)
+for subset in split_labels_2:
+  info_gain -= gini(subset)
+print(info_gain)
+
