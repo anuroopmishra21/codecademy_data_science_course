234001210bcfd7a66ebaa8f4b4e9b155a284a404
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-21 17:04:37 +0530

    Update dr_dirac_statistic_midterm_project_bayes_theorem_module_20.py

--- dr_dirac_statistic_midterm_project_bayes_theorem_module_20.py
+++ dr_dirac_statistic_midterm_project_bayes_theorem_module_20.py
@@ -1,2 +1,6 @@
 import numpy as np
 p_knows_material = 0.6
+p_correct_given_knows_material = 0.85
+p_correct = (0.85*0.6)+(0.20*0.40)
+p_knows_material_given_correct = (p_correct_given_knows_material * p_knows_material) / p_correct
+print(p_knows_material_given_correct)
