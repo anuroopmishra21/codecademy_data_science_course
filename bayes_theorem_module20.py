781cf70fcc23ae7a535fad295b4e47fbf5cfda08
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-21 17:01:05 +0530

    Update bayes_theorem_module20.py

--- bayes_theorem_module20.py
+++ bayes_theorem_module20.py
@@ -15,3 +15,12 @@ p_disease = 0.00001
 p_positive = (0.00001 * 0.99) + (0.01 * 0.99999)
 p_disease_given_positive = (p_positive_given_disease * p_disease) / (p_positive)
 print(p_disease_given_positive)
+
+import numpy as np
+a = "spam"
+b = "enhancement"
+p_spam = 0.20
+p_enhancement_given_spam = 0.05
+p_enhancement = (0.05*0.2)+(0.001*0.8)
+p_spam_enhancement = (p_enhancement_given_spam*p_spam)/p_enhancement
+print(p_spam_enhancement)
