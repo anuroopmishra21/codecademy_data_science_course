a262ec425688d59212f5c4881d79011c72b5bf29
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-21 16:42:30 +0530

    Update bayes_theorem_module20.py

--- bayes_theorem_module20.py
+++ bayes_theorem_module20.py
@@ -8,3 +8,10 @@ p_disease_and_correct = 0.00001 * 0.99
 p_no_disease_and_incorrect = 0.99999 * 0.01
 print(p_disease_and_correct)
 print(p_no_disease_and_incorrect)
+
+import numpy as np
+p_positive_given_disease = 0.99
+p_disease = 0.00001
+p_positive = (0.00001 * 0.99) + (0.01 * 0.99999)
+p_disease_given_positive = (p_positive_given_disease * p_disease) / (p_positive)
+print(p_disease_given_positive)
