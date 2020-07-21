44471429358648814a50e430cbd94c763f2ca088
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-21 16:35:15 +0530

    Update bayes_theorem_module20.py

--- bayes_theorem_module20.py
+++ bayes_theorem_module20.py
@@ -2,3 +2,9 @@ third_child = "independent"
 
 import numpy as np
 p_rain_and_gym = 0.30 * 0.60
+
+import numpy as np
+p_disease_and_correct = 0.00001 * 0.99
+p_no_disease_and_incorrect = 0.99999 * 0.01
+print(p_disease_and_correct)
+print(p_no_disease_and_incorrect)
