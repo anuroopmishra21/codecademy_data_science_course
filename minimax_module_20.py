9395f6c940185751916b55c67ccd17f1aa5cfa60
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-22 10:16:14 +0530

    Update minimax_module_20.py

--- minimax_module_20.py
+++ minimax_module_20.py
@@ -56,3 +56,14 @@ print(evaluate_board(x_won))
 print(evaluate_board(o_won))
 print(evaluate_board(tie))
 
+from tic_tac_toe import *
+from copy import deepcopy
+my_board = [
+	["1", "2", "X"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+]
+new_board = deepcopy(my_board)
+select_space(new_board, 5, 'O')
+print_board(my_board)
+print_board(new_board)
