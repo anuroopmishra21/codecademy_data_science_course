ee892a0b7c470f05765f45437ae13c19d920dbae
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-22 14:29:41 +0530

    Update advanced_minimax_module20.py

--- advanced_minimax_module20.py
+++ advanced_minimax_module20.py
@@ -32,3 +32,30 @@ def minimax(input_board, is_maximizing, depth):
       best_move = move
   return [best_value, best_move]
 print(minimax(new_board, True, 3))
+
+from connect_four import *
+import random
+random.seed(108)
+def evaluate_board(board):
+    if has_won(board, "X"):
+      return float("Inf")
+    elif has_won(board, "O"):
+      return -float("Inf")
+    else:
+      num_top_x = 0
+      num_top_o = 0
+      for column in board:
+        for square in column:
+          if square == 'X':
+            num_top_x +=1
+            break
+          if square == 'O':
+            num_top_o +=1
+            break
+      return num_top_x - num_top_o
+print_board(board_one)
+print_board(board_two)
+print_board(board_three)
+print(evaluate_board(board_one))
+print(evaluate_board(board_two))
+print(evaluate_board(board_three))
