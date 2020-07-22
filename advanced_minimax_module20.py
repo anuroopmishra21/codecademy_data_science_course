b6bb4ab6e044569fa6fcb0b794dfc29e61a0953e
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-22 14:44:38 +0530

    Update advanced_minimax_module20.py

--- advanced_minimax_module20.py
+++ advanced_minimax_module20.py
@@ -92,3 +92,28 @@ def minimax(input_board, is_maximizing, depth, alpha, beta):
   return [best_value, best_move, alpha, beta]
 print_board(board)
 print(minimax(board, True, 6, -float('Inf'), float('Inf')))
+
+from connect_four import *
+def two_ai_game():
+    my_board = make_board()
+    while not game_is_over(my_board):
+      # Fill in the third parameter for the first player's "intelligence"
+      result = minimax(my_board, True, 4, -float("Inf"), float("Inf"))
+      print( "X Turn\nX selected ", result[1])
+      print(result[1])
+      select_space(my_board, result[1], "X")
+      print_board(my_board)
+      if not game_is_over(my_board):
+        #Fill in the third parameter for the second player's "intelligence"
+        result = minimax(my_board, False, 4, -float("Inf"), float("Inf"))
+        print( "O Turn\nO selected ", result[1])
+        print(result[1])
+        select_space(my_board, result[1], "O")
+        print_board(my_board)
+    if has_won(my_board, "X"):
+        print("X won!")
+    elif has_won(my_board, "O"):
+        print("O won!")
+    else:
+        print("It's a tie!")
+two_ai_game()
