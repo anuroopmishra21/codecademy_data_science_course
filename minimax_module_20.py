f7a398ce637d773356d454158fc129bbfc6eae6a
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-22 10:53:01 +0530

    Update minimax_module_20.py

--- minimax_module_20.py
+++ minimax_module_20.py
@@ -148,3 +148,55 @@ def minimax(input_board, is_maximizing):
   return best_value
 print(minimax(x_winning, True))
 print(minimax(o_winning, True))
+
+from tic_tac_toe import *
+from copy import deepcopy
+def game_is_over(board):
+  return has_won(board, "X") or has_won(board, "O") or len(available_moves(board)) == 0
+def evaluate_board(board):
+  if has_won(board, "X"):
+    return 1
+  elif has_won(board, "O"):
+    return -1
+  else:
+    return 0
+new_game = [
+	["1", "2", "3"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+]
+x_winning = [
+	["X", "2", "O"],
+	["4", "O", "6"],
+	["7", "8", "X"]
+]
+o_winning = [
+	["X", "X", "O"],
+	["4", "X", "6"],
+	["7", "O", "O"]
+]
+def minimax(input_board, is_maximizing):
+  # Base case - the game is over, so we return the value of the board
+  if game_is_over(input_board):
+    return [evaluate_board(input_board), ""]
+  if is_maximizing == True:
+    best_value = -float("Inf")
+    best_move = ""
+    symbol = "X"
+  else:
+    best_value = float("Inf")
+    best_move = ""
+    symbol = "O"
+  for move in available_moves(input_board):
+    new_board = deepcopy(input_board)
+    select_space(new_board, move, symbol)
+    hypothetical_value = minimax(new_board, not is_maximizing)[0]
+    if is_maximizing == True and hypothetical_value > best_value:
+      best_value = hypothetical_value
+      best_move = move
+    if is_maximizing == False and hypothetical_value < best_value:
+      best_value = hypothetical_value
+      best_move = move
+  return [best_value, best_move]
+print(minimax(x_winning, True))
+print(minimax(o_winning, False))
