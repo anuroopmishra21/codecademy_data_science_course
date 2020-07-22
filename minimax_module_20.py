15d21015b978a36e5b61cfc3cf9189f7d1c35bf9
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-22 14:09:11 +0530

    Update minimax_module_20.py

--- minimax_module_20.py
+++ minimax_module_20.py
@@ -200,3 +200,16 @@ def minimax(input_board, is_maximizing):
   return [best_value, best_move]
 print(minimax(x_winning, True))
 print(minimax(o_winning, False))
+
+from tic_tac_toe import *
+my_board = [
+	["1", "2", "3"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+]
+while not game_is_over(my_board):
+  select_space(my_board, minimax(my_board, True)[1], "X")
+  print_board(my_board)
+  if not game_is_over(my_board):
+    select_space(my_board, minimax(my_board, False)[1], "O")
+    print_board(my_board) 
