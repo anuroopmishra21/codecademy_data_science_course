55faf76b1b2e8de2426d026f6e5d7b7b5996a07f
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-23 23:31:58 +0530

    Update k_means_clustering_module22.py

--- k_means_clustering_module22.py
+++ k_means_clustering_module22.py
@@ -41,3 +41,17 @@ iris = datasets.load_iris()
 print(iris.data)
 print(iris.target)
 print(iris.DESCR)
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+from sklearn import datasets
+iris = datasets.load_iris()
+# Store iris.data
+samples = iris.data
+# Create x and y
+x = samples[:,0]
+y = samples[:,1]
+# Plot x and y
+plt.scatter(x,y,alpha = 0.5)
+# Show the plot
+plt.show()
