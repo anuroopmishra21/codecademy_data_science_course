784f48bf0e71ba0421a83fa2dba5cafc94851bdf
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 01:51:33 +0530

    Update perceptron_neural_networks_module23.py

--- perceptron_neural_networks_module23.py
+++ perceptron_neural_networks_module23.py
@@ -35,3 +35,26 @@ class Perceptron:
 cool_perceptron = Perceptron()
 print(cool_perceptron.weighted_sum([24, 55]))
 print(cool_perceptron.activation(52))
+
+class Perceptron:
+  def __init__(self, num_inputs=2, weights=[1,1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
+  def weighted_sum(self, inputs):
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += self.weights[i]*inputs[i]
+    return weighted_sum
+  def activation(self, weighted_sum):
+    if weighted_sum >= 0:
+      return 1
+    if weighted_sum < 0:
+      return -1
+  def training(self, training_set):
+    for inputs in training_set:                   
+      prediction = self.activation(self.weighted_sum(inputs))
+      actual = training_set[inputs]
+      error = actual - prediction
+cool_perceptron = Perceptron()
+print(cool_perceptron.weighted_sum([24, 55]))
+print(cool_perceptron.activation(52))
