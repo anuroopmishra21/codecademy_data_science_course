0d88894e583f3b0aaa52459ed1713fcd7d8a47e3
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 02:03:55 +0530

    Update perceptron_neural_networks_module23.py

--- perceptron_neural_networks_module23.py
+++ perceptron_neural_networks_module23.py
@@ -58,3 +58,35 @@ class Perceptron:
 cool_perceptron = Perceptron()
 print(cool_perceptron.weighted_sum([24, 55]))
 print(cool_perceptron.activation(52))
+
+class Perceptron:
+  def __init__(self, num_inputs=2, weights=[1,1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
+  def weighted_sum(self, inputs):
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += self.weights[i]*inputs[i]
+    return weighted_sum
+  def activation(self, weighted_sum):
+    if weighted_sum >= 0:
+      return 1
+    if weighted_sum < 0:
+      return -1
+  def training(self, training_set):
+    foundLine = False
+    while not foundLine:
+      total_error = 0
+      for inputs in training_set:
+        prediction = self.activation(self.weighted_sum(inputs))
+        actual = training_set[inputs]
+        error = actual - prediction
+        total_error += abs(error)
+        for i in range(self.num_inputs):
+          self.weights[i] += (error*inputs[i])
+      if total_error == 0:
+        foundLine = True
+    print(self.weights)
+cool_perceptron = Perceptron()
+small_training_set = {(0,3):1, (3,0):-1, (0,-3):-1, (-3,0):1}
+cool_perceptron.training(small_training_set)
