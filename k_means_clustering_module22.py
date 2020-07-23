24c3281de9d33d56121bf5680015d7afcc4262db
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-23 23:54:20 +0530

    Update k_means_clustering_module22.py

--- k_means_clustering_module22.py
+++ k_means_clustering_module22.py
@@ -79,3 +79,32 @@ plt.scatter(x , y, alpha =0.5)
 plt.scatter(centroids_x, centroids_y)
 # Display plot
 plt.show()
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+iris = datasets.load_iris()
+samples = iris.data
+x = samples[:,0]
+y = samples[:,1]
+sepal_length_width = np.array(list(zip(x, y)))
+# Step 1: Place K random centroids
+k = 3
+centroids_x = np.random.uniform(min(x), max(x), size=k)
+centroids_y = np.random.uniform(min(y), max(y), size=k)
+centroids = np.array(list(zip(centroids_x, centroids_y)))
+# Step 2: Assign samples to nearest centroid
+def distance(a,b):
+  distance = 0
+  for i in range(len(a)):
+    distance += (a[i]-b[i])**2
+  return distance**0.5
+labels = np.zeros(len(samples))
+distances = np.zeros(k)
+for i in range(len(samples)):
+  for j in range(k):
+    distances[j] = distance(sepal_length_width[i], centroids[j])
+  cluster = np.argmin(distances)
+  labels[i] = cluster
+print(labels)
