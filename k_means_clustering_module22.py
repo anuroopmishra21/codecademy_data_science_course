a18aa4611db34f06ecf293d4a7aa8d7cb39f548f
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-23 23:38:10 +0530

    Update k_means_clustering_module22.py

--- k_means_clustering_module22.py
+++ k_means_clustering_module22.py
@@ -55,3 +55,27 @@ y = samples[:,1]
 plt.scatter(x,y,alpha = 0.5)
 # Show the plot
 plt.show()
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+iris = datasets.load_iris()
+samples = iris.data
+x = samples[:,0]
+y = samples[:,1]
+# Number of clusters
+k = 3
+# Create x coordinates of k random centroids
+centroids_x = np.random.uniform(min(x), max(x), 3)
+# Create y coordinates of k random centroids
+centroids_y = np.random.uniform(min(y), max(y), 3)
+# Create centroids array
+centroids = np.array(list(zip(centroids_x, centroids_y)))
+print(centroids)
+# Make a scatter plot of x, y
+plt.scatter(x , y, alpha =0.5)
+# Make a scatter plot of the centroids
+plt.scatter(centroids_x, centroids_y)
+# Display plot
+plt.show()
