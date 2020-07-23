a51888cc3025bbe634ce4a82ec938202e07bfb6c
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 01:32:59 +0530

    Create perceptron_neural_networks_module23.py

---
+++ perceptron_neural_networks_module23.py
@@ -0,0 +1,7 @@
+class Perceptron:
+  def __init__(self, num_inputs=2, weights=[1,1]):
+    # complete the default constructor method
+    self.num_inputs = num_inputs
+    self.weights = weights
+cool_perceptron = Perceptron()
+print(cool_perceptron)
