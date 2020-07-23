0084fd6fb80d44acc29a113cd78efb1a7f340db5
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 00:31:56 +0530

    Update k_means_clustering_module22.py

--- k_means_clustering_module22.py
+++ k_means_clustering_module22.py
@@ -218,3 +218,18 @@ model = KMeans(n_clusters = 3)
 model.fit(samples)
 labels = model.predict(samples)
 print(labels)
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.cluster import KMeans
+iris = datasets.load_iris()
+samples = iris.data
+model = KMeans(n_clusters=3)
+model.fit(samples)
+new_samples = np.array([[5.7, 4.4, 1.5, 0.4],
+   [6.5, 3. , 5.5, 0.4],
+   [5.8, 2.7, 5.1, 1.9]])
+print(new_samples)
+print(model.predict(new_samples))
