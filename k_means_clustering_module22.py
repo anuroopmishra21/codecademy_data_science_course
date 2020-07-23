ac6754960929b1452e07ca628be83b145369db3f
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 00:46:04 +0530

    Update k_means_clustering_module22.py

--- k_means_clustering_module22.py
+++ k_means_clustering_module22.py
@@ -275,3 +275,22 @@ df = pd.DataFrame({'labels': labels, 'species': species})
 print(df)
 ct = pd.crosstab(df['labels'], df['species'])
 print(ct)
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.cluster import KMeans
+iris = datasets.load_iris()
+samples = iris.data
+num_clusters = list(range(1,9))
+inertias = []
+for i in num_clusters:
+  model = KMeans(n_clusters = i)
+  model.fit(samples)
+  inertias.append(model.inertia_)
+plt.plot(num_clusters, inertias, '-o')
+plt.xlabel('Number of Clusters(k)')
+plt.ylabel('Inertia')
+plt.show()
