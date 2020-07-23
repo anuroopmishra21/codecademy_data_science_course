67b647f30abd95d0fb76d860699bf7244b3e79b9
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 00:00:34 +0530

    Update k_means_clustering_module22.py

--- k_means_clustering_module22.py
+++ k_means_clustering_module22.py
@@ -108,3 +108,46 @@ for i in range(len(samples)):
   cluster = np.argmin(distances)
   labels[i] = cluster
 print(labels)
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from copy import deepcopy
+iris = datasets.load_iris()
+samples = iris.data
+samples = iris.data
+x = samples[:,0]
+y = samples[:,1]
+sepal_length_width = np.array(list(zip(x, y)))
+# Step 1: Place K random centroid
+k = 3
+centroids_x = np.random.uniform(min(x), max(x), size=k)
+centroids_y = np.random.uniform(min(y), max(y), size=k)
+centroids = np.array(list(zip(centroids_x, centroids_y)))
+# Step 2: Assign samples to nearest centroid
+def distance(a, b):
+  one = (a[0] - b[0]) **2
+  two = (a[1] - b[1]) **2
+  distance = (one+two) ** 0.5
+  return distance
+# Cluster labels for each point (either 0, 1, or 2)
+labels = np.zeros(len(samples))
+# Distances to each centroid
+distances = np.zeros(k)
+for i in range(len(samples)):
+  distances[0] = distance(sepal_length_width[i], centroids[0])
+  distances[1] = distance(sepal_length_width[i], centroids[1])
+  distances[2] = distance(sepal_length_width[i], centroids[2])
+  cluster = np.argmin(distances)
+  labels[i] = cluster
+# Step 3: Update centroids
+centroids_old = deepcopy(centroids)
+for i in range(k):
+  points = []
+  for j in range(len(sepal_length_width)):
+    if labels[j] == i:
+      points.append(sepal_length_width[j])
+  centroids[i] = np.mean(points, axis =0)
+print(centroids_old)
+print(centroids)
