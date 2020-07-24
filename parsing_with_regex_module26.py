68e45244c86aae906ef38790a684f5a1f6ada58d
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-25 00:09:59 +0530

    Update parsing_with_regex_module26.py

--- parsing_with_regex_module26.py
+++ parsing_with_regex_module26.py
@@ -85,3 +85,23 @@ for sentence in pos_tagged_oz:
 # store and print the most common vp-chunks here
 most_common_vp_chunks = vp_chunk_counter(vp_chunked_oz)
 print(most_common_vp_chunks)
+
+from nltk import RegexpParser, Tree
+from pos_tagged_oz import pos_tagged_oz
+# define chunk grammar to chunk an entire sentence together
+grammar = "Chunk: {<.*>+}"
+# create RegexpParser object
+parser = RegexpParser(grammar)
+# chunk the pos-tagged sentence at index 230 in pos_tagged_oz
+chunked_dancers = parser.parse(pos_tagged_oz[230])
+print(chunked_dancers)
+# define noun phrase chunk grammar using chunk filtering here
+chunk_grammar = """NP: {<.*>+}
+                       }<VB.?|IN>+{"""
+# create RegexpParser object here
+chunk_parser = RegexpParser(chunk_grammar)
+# chunk and filter the pos-tagged sentence at index 230 in pos_tagged_oz here
+filtered_dancers = chunk_parser.parse(pos_tagged_oz[230])
+print(filtered_dancers)
+# pretty_print the chunked and filtered sentence here
+Tree.fromstring(str(filtered_dancers)).pretty_print()
