f345c70a070cf7bb3cf42a082a597b47beb02c63
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-25 01:31:42 +0530

    Update bag_of_words_module26.py

--- bag_of_words_module26.py
+++ bag_of_words_module26.py
@@ -70,3 +70,36 @@ spam_classifier.fit(training_vectors, training_labels)
 predictions = spam_classifier.score(test_vectors, test_labels)
 print("The predictions for the test data were {0}% accurate.\n\nFor example, '{1}' was classified as {2}.\n\nMeanwhile, '{3}' was classified as {4}.".format(predictions * 100, test_docs[0], spam_or_not(test_labels[0]), test_docs[10], spam_or_not(test_labels[10])))
 
+from spam_data import training_spam_docs, training_doc_tokens, training_labels, test_labels, test_spam_docs, training_docs, test_docs
+from sklearn.naive_bayes import MultinomialNB
+# Import CountVectorizer from sklearn:
+from sklearn.feature_extraction.text import CountVectorizer
+# Define bow_vectorizer:
+bow_vectorizer = CountVectorizer()
+# Define training_vectors:
+training_vectors = bow_vectorizer.fit_transform(training_docs)
+# Define test_vectors:
+test_vectors = bow_vectorizer.transform(test_docs)
+spam_classifier = MultinomialNB()
+def spam_or_not(label):
+  return "spam" if label else "not spam"
+spam_classifier.fit(training_vectors, training_labels)
+predictions = spam_classifier.score(test_vectors, test_labels)
+print("The predictions for the test data were {0}% accurate.\n\nFor example, '{1}' was classified as {2}.\n\nMeanwhile, '{3}' was classified as {4}.".format(predictions * 100, test_docs[7], spam_or_not(test_labels[7]), test_docs[15], spam_or_not(test_labels[15])))
+
+from preprocessing import preprocess_text
+from nltk.util import ngrams
+from collections import Counter
+text = "It's exciting to watch flying fish after a hard day's work. I don't know why some fish prefer flying and other fish would rather swim. It seems like the fish just woke up one day and decided, 'hey, today is the day to fly away.'"
+tokens = preprocess_text(text)
+# Bigram approach:
+bigrams_prepped = ngrams(tokens, 2)
+bigrams = Counter(bigrams_prepped)
+print("Three most frequent word sequences and the number of occurrences according to Bigrams:")
+print(bigrams.most_common(3))
+# Bag-of-Words approach:
+# Define bag_of_words here:
+bag_of_words = Counter(tokens)
+print("\nThree most frequent words and number of occurrences according to Bag-of-Words:")
+most_common_three = Counter.most_common(bag_of_words,3)
+print(most_common_three)
