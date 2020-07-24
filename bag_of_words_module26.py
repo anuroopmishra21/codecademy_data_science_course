80f65bd4395d4b97135d0f604109f03e77eec5c3
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-25 01:05:18 +0530

    Update bag_of_words_module26.py

--- bag_of_words_module26.py
+++ bag_of_words_module26.py
@@ -11,3 +11,17 @@ def text_to_bow(some_text):
   return bow_dictionary
 print(text_to_bow("I love fantastic flying fish. These flying fish are just ok, so maybe I will find another few fantastic fish..."))
 
+from preprocessing import preprocess_text
+# Define create_features_dictionary() below:
+def create_features_dictionary(documents):
+  features_dictionary = {}
+  merged = " ".join(documents)
+  tokens = preprocess_text(merged)
+  index = 0
+  for token in tokens:
+    if token not in features_dictionary:
+      features_dictionary[token] = index
+      index += 1
+  return features_dictionary, tokens
+training_documents = ["Five fantastic fish flew off to find faraway functions.", "Maybe find another five fantastic fish?", "Find my fish with a function please!"]
+print(create_features_dictionary(training_documents)[0])
