f64adfee4759d9b53428bcebdc6f8df47931e3d6
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-07-24 23:23:17 +0530

    Update parsing_with_regex_module26.py

--- parsing_with_regex_module26.py
+++ parsing_with_regex_module26.py
@@ -13,3 +13,15 @@ print(match_1)
 result_2 = re.match("[A-Za-z]{7}", character_2)
 print(result_2)
 
+import re
+# import L. Frank Baum's The Wonderful Wizard of Oz
+oz_text = open("the_wizard_of_oz_text.txt",encoding='utf-8').read().lower()
+# search oz_text for an occurrence of 'wizard' here
+found_wizard = re.search('wizard', oz_text)
+print(found_wizard)
+# find all the occurrences of 'lion' in oz_text here
+all_lions = re.findall('lion', oz_text)
+print(all_lions)
+# store and print the length of all_lions here
+number_lions = len(all_lions)
+print(number_lions)
